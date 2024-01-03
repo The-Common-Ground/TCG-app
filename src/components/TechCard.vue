7bce25f83336269a47e359c544aa185d59db5807
Harry-cheong <harrycheongjunkai@gmail.com>
2024-01-03 18:08:45 +0800

    contribute pg - tech stack diagram

---
+++ src/components/TechCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card color="black" max-width="250px">
+    <v-card-title>
+      <v-icon>{{ icon }}</v-icon>
+      <v-spacer></v-spacer>
+      <a class="text-h4 bold">{{ title }}</a>
+      <v-spacer></v-spacer>
+      <v-container class="pt-1">
+        <v-col>
+          <a class="text-subtitle-2 text-wrap"> {{ subtitle }}</a>
+        </v-col>
+      </v-container>
+    </v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    subtitle: String,
+    icon: String,
+  },
+};
+</script>
+
+<style>
+.icon {
+  width: 50px;
+  height: 50px;
+}
+</style>
--- src/views/Contribute.vue
+++ src/views/Contribute.vue
@@ -15,7 +15,7 @@
           </v-card-title>
           <v-card-action>
             <v-container>
-              <v-row justify="left">
+              <v-row class="w-50" justify="left">
                 <v-col cols="auto">
                   <v-btn variant="outlined" class="icon-btn" density="default">
                     <v-icon class="pr-2"> mdi-github</v-icon>
@@ -41,16 +41,55 @@
     </v-row>
 
     <v-row justify="center" align="center" class="tech-stack">
-      <v-card width="100%" variant="flat">
+      <v-card width="88%" variant="flat" height="900px">
         <v-card-title class="text-h3 pb-8 text-centre">
           Our Tech Stack
         </v-card-title>
-        <v-carousel show-arrows="hover" hide-delimiter-background>
+        <v-carousel show-arrows="hover" height="100%" hide-delimiter-background>
           <v-carousel-item v-for="(slide, i) in slides" :key="i">
-            <v-card class="rounded-xl" color="black" height="100%" width="100%">
+            <v-card class="rounded-xl" color="black" height="90%" width="100%">
               <v-card-title class="pa-12">
                 <v-icon class="pr-4 pb-2">mdi-code-tags</v-icon>
-                <a class="text-h4">Frontend</a>
+                <a class="text-h4">{{ slide.name }}</a>
+                <v-card class="mt-5 mx-auto" color="black" flex max-width="800">
+                  <v-container>
+                    <v-row>
+                      <v-col v-if="slide.noOfItems >= 1">
+                        <tech-card
+                          :title="slide.items[0].title"
+                          :subtitle="slide.items[0].subtitle"
+                          :icon="slide.items[0].icon"
+                        >
+                        </tech-card>
+                      </v-col>
+                      <v-col v-if="slide.noOfItems >= 2">
+                        <tech-card
+                          :title="slide.items[1].title"
+                          :icon="slide.items[1].icon"
+                          :subtitle="slide.items[1].subtitle"
+                        ></tech-card>
+                      </v-col>
+                      <v-col v-if="slide.noOfItems >= 3">
+                        <tech-card
+                          :title="slide.items[2].title"
+                          :icon="slide.items[2].icon"
+                          :subtitle="slide.items[2].subtitle"
+                        ></tech-card>
+                      </v-col>
+                    </v-row>
+                    <v-row v-if="slide.noOfItems > 3">
+                      <v-col v-if="slide.noOfItems >= 4">
+                        <tech-card
+                          :title="slide.items[3].title"
+                          :icon="slide.items[3].icon"
+                          :subtitle="slide.items[3].subtitle"
+                        ></tech-card>
+                      </v-col>
+                      <v-col v-if="slide.noOfItems >= 5">Tech 5</v-col>
+                      <v-col v-if="slide.noOfItems >= 6">Tech 6</v-col>
+                    </v-row>
+                  </v-container>
+                </v-card>
               </v-card-title>
             </v-card>
           </v-carousel-item>
@@ -61,12 +100,74 @@
 </template>
 
 <script>
+import TechCard from "../components/TechCard.vue";
+
 export default {
   data() {
     return {
-      slides: ["Frontend", "Backend"],
+      slides: [
+        {
+          name: "Frontend",
+          noOfItems: 4,
+          items: [
+            {
+              icon: "mdi-nodejs",
+              title: "JavaScript",
+              subtitle: "Programming Language",
+            },
+            {
+              icon: "mdi-vuejs",
+              title: "Vue.js",
+              subtitle:
+                "Vue.js is a common frontend framework for building user interfaces",
+            },
+            {
+              icon: "mdi-language-html5",
+              title: "HTML",
+              subtitle:
+                "Hyper Text Mark-up Language (HTML) dictates the structure of the website",
+            },
+            {
+              icon: "mdi-language-css3",
+              title: "CSS",
+              subtitle:
+                "Cascading Style Sheet (CSS) for element design, styling and transitions",
+            },
+          ],
+        },
+        {
+          name: "Backend",
+          noOfItems: 4,
+          items: [
+            {
+              icon: "mdi-language-python",
+              title: "Python",
+              subtitle: "Programming Language",
+            },
+            {
+              icon: "mdi-laptop",
+              title: "Flask",
+              subtitle:
+                "Flask is a Python web framework which provides useful tools and features to create apps",
+            },
+            {
+              icon: "mdi-laptop",
+              title: "Firebase",
+              subtitle:
+                "Firebase is a backend cloud computing services and application platform provided by Google",
+            },
+            {
+              icon: "mdi-laptop",
+              title: "MongoDB",
+              subtitle:
+                "MongoDB is a cross-platform document-oriented database program",
+            },
+          ],
+        },
+      ],
     };
   },
+  components: { TechCard },
 };
 </script>
 <style>
@@ -78,5 +179,10 @@ export default {
 .tech-stack {
   padding-top: 100px;
   padding-bottom: 100px;
+}
+
+.icon {
+  width: 50px;
+  height: 50px;
 }
 </style>
