17d1670c5060b787a27acf5eca6ec1b537789407
Harry-cheong <harrycheongjunkai@gmail.com>
2024-04-15 20:00:02 +0800

    update footer

--- src/components/ThePageFooter.vue
+++ src/components/ThePageFooter.vue
@@ -1,17 +1,15 @@
 <template>
   <v-footer inset>
-    <v-sheet width="100%" color="#050038" height="80px">
-      <v-container variant="bg-surface-variant">
-        <v-col class="text-left">
-          <v-sheet color="white" height="1" width="100%"> </v-sheet>
-          <v-chip variant="text" class="mt-1">
-            <v-btn max-width="5" variant="text">
-              <v-icon href="https://instagram.com">mdi-instagram</v-icon>
-            </v-btn>
-            <a class="ml-n4">@The Common Ground</a>
-          </v-chip>
-        </v-col>
-      </v-container>
+    <v-sheet
+      width="100%"
+      color="#050038"
+      height="80px"
+      class="d-flex align-center justify-center"
+    >
+      <v-btn max-width="5" variant="text">
+        <v-icon>mdi-instagram</v-icon>
+      </v-btn>
+      <p class="ml-n2">Follow us on our Instagram @TheCommonGr0und</p>
     </v-sheet>
   </v-footer>
 </template>
